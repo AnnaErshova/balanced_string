c52a383270f806889b86cc8f26a2478c3ac39f1a
AnnaErshova <anna.ershova@gmail.com>
2015-08-19 12:04:49 -0400

    improves testing suite examples for unbalanced string

--- Flatiron/ProgrammingChallenges/balanced_string/spec/balanced_string_spec.rb
+++ Flatiron/ProgrammingChallenges/balanced_string/spec/balanced_string_spec.rb
@@ -24,23 +24,31 @@ describe 'balanced?' do
 
   context 'string is unbalanced:' do
     it 'returns "false" when the string is not balanced' do
-      string_4 = balanced?('strin(g')
+      string_4 = balanced?('strin())g')
       expect(string_4).to eq(false)
     end
     it 'returns "false" when the string is not balanced' do
-      string_5 = balanced?('st()((((rin(g')
+      string_5 = balanced?('st()((((rin()g')
       expect(string_5).to eq(false)
     end
   end
 
   context 'string is invalid:' do
-    it 'returns "invalid" when the first parentheses is )' do
+    it 'returns "invalid" when the first parentheses is ) even if the string would be balanced otherwise' do
       string_6 = balanced?('str)(()ing')
       expect(string_6).to eq("invalid")
     end
-    it 'returns "invalid" when the last parentheses is (' do
-      string_7 = balanced?('str())((ing')
+    it 'returns "invalid" when the first parentheses is )' do
+      string_7 = balanced?('st))r(()ing')
       expect(string_7).to eq("invalid")
+    end
+    it 'returns "invalid" when the last parentheses is ( even if the string would be balanced otherwise' do
+      string_8 = balanced?('str())()(ing')
+      expect(string_8).to eq("invalid")
+    end
+    it 'returns "invalid" when the last parentheses is (' do
+      string_9 = balanced?('str())((ing')
+      expect(string_9).to eq("invalid")
     end
   end
 
